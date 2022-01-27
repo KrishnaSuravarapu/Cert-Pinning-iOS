484f0d29faa9cee69fd5e0a8b70a5c13bc77ae8f
KrishnaSuravarapu <krishna.su@browserstack.com>
2022-01-27 10:47:57 +0530

    Initial Commit

---
+++ Cert Pinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cert Pinning
+//
+//  Created by Krishna Suravarapu on 27/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
